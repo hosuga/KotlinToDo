8fb94f6c201cc61472a86659bc8e0b72fbbb4cbd
大須賀寛人 <hosuga@itrealize.co.jp>
2021-04-04 22:33:17 +0900

    編集フォーム追加

--- app/src/main/java/com/example/kotlintodo/EditActivity.kt
+++ app/src/main/java/com/example/kotlintodo/EditActivity.kt
@@ -1,12 +1,22 @@
 package com.example.kotlintodo
 
-import android.graphics.Color
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlintodo.model.EditorMode
 
 class EditActivity : AppCompatActivity() {
+    val ERROR_MESSAGES = mapOf(
+        "HEADER" to "ERROR!",
+        "EMPTY_TITLE" to "件名を入力して下さい。",
+        "TOO_LONG_TITLE" to "20文字以内で入力して下さい。"
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,9 +29,16 @@ class EditActivity : AppCompatActivity() {
 
         val saveButton = findViewById<Button>(R.id.saveButton)
         val cancelButton = findViewById<Button>(R.id.cancelButton)
+        val editTitleForm = findViewById<EditText>(R.id.editTitleForm)
 
         saveButton.setOnClickListener {
-            finish()
+            val title = editTitleForm.text.toString()
+            Log.d("title", title)
+            if (validate((title))) {
+                // TODO: DB更新
+                finish()
+            }
+
         }
 
         cancelButton.setOnClickListener {
@@ -33,5 +50,25 @@ class EditActivity : AppCompatActivity() {
             )
             cancelConfirmDialog.show(supportFragmentManager, "cancel_confirm_dialog")
         }
+    }
+
+    private fun validate(title: String): Boolean {
+        if (isEmpty(title.trim())) {
+            AlertDialog.Builder(this)
+                .setTitle(ERROR_MESSAGES["HEADER"])
+                .setMessage(ERROR_MESSAGES["EMPTY_TITLE"])
+                .setPositiveButton("OK"){ _, _ -> }
+                .show()
+            return false
+        }
+        if (title.length > 20) {
+            AlertDialog.Builder(this)
+                .setTitle(ERROR_MESSAGES["HEADER"])
+                .setMessage(ERROR_MESSAGES["TOO_LONG_TITLE"])
+                .setPositiveButton("OK"){ _, _ -> }
+                .show()
+            return false
+        }
+        return true
     }
 }
