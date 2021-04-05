74f91a055ee3df5e3cdd74ffb5c755d85033adc4
大須賀寛人 <hosuga@itrealize.co.jp>
2021-04-05 10:42:48 +0900

    アクセサーの追加

--- app/src/main/java/com/example/kotlintodo/MainActivity.kt
+++ app/src/main/java/com/example/kotlintodo/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlintodo.model.EditorMode
-import com.example.kotlintodo.realm.ToDo
+import com.example.kotlintodo.realm.ToDoAccessor
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
@@ -22,8 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         // dummyDate
         val todos = Array<String>(50){"Title$it"}
-        val test = ToDo().read()
 
+//        ToDoAccessor().create("test01")
+        val test = ToDoAccessor().getAll()
         Log.d("todos", test.toString())
 
 
--- app/src/main/java/com/example/kotlintodo/realm/ToDoAccessor.kt
+++ app/src/main/java/com/example/kotlintodo/realm/ToDoAccessor.kt
@@ -3,12 +3,34 @@ package com.example.kotlintodo.realm
 import com.example.kotlintodo.db.ToDo
 import io.realm.Realm
 import io.realm.RealmResults
+import io.realm.kotlin.createObject
 import io.realm.kotlin.where
 
-class ToDo {
+class ToDoAccessor {
     var realm = Realm.getDefaultInstance()
 
-    fun read(): RealmResults<ToDo>? {
+
+    fun generateId(): Int {
+        var nextId = 1
+
+        val lastId = realm.where<ToDo>().max("id")
+
+        if(lastId != null) {
+            nextId = lastId.toInt() + 1
+        }
+
+        return nextId
+    }
+
+
+    fun create(title:String) {
+        var todo = realm.createObject<ToDo>(generateId())
+        todo.title = title
+        realm.copyToRealm(todo)
+    }
+
+
+    fun getAll(): RealmResults<ToDo>? {
         val todos = realm.where<ToDo>().findAll()
         return todos
     }
